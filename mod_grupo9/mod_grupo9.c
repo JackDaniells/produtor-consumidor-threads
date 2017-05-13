7af7a22f3a7a7c2c33d5e9c5074a7e2868db6115
jackdaniells <danielk.kock@live.com>
2017-05-13 02:30:19 -0300

    Inserido semáforos - agora e 78000 sem condição de corrida carai

--- mod_grupo9/mod_grupo9.c
+++ mod_grupo9/mod_grupo9.c
@@ -13,7 +13,9 @@ int setup = 0;
 //cria as threads
 pthread_t workThread[PROCESSORS_COUNT], prodThread;
 //cria os mutex
-pthread_mutex_t lockDeliver, lockCounter;
+pthread_mutex_t lockDeliver, lockCounter, lockWorker;
+//cria os semáforos
+sem_t lockEmpty, lockFull;
 //cria o buffer
 rock_t bufferRock[PROCESSORS_COUNT];
 //cria as variáveis globais do buffer
@@ -21,95 +23,94 @@ int posReadBuffer = 0, posWriteBuffer = 0, countRock = 0;
 
 
 
-//função que manipula o contador, criada pra evitar condição de corrida no acesso a variavel counter
-int CountRock(int n){
 
-    //variável criada pra evitar o acesso a variável "counter" durante a execução paralela entre threads
-    int back = -1;
-
-    //trava as threads enquanto incrementa o contador
-    pthread_mutex_lock(&lockCounter);
-    countRock = countRock + n;
-    back = countRock;
-    pthread_mutex_unlock(&lockCounter);
-    printf("%d\n", back);
-
-    //retorna o valor do contador
-    return back;
-}
-
-
-/*************
-*   Threads
-*************/
-
+/*************************************************************
+*
+*                           Threads
+*
+/*************************************************************/
 //thread produtora
 static void* prod() {
 
     while (cancelled == 0) {
 
-        //trava a thread se o buffer estiver cheio
-        while(countRock >= PROCESSORS_COUNT);
-
-        //incrementa o posWrite
-        posWriteBuffer++;
-        if(posWriteBuffer == PROCESSORS_COUNT ) posWriteBuffer = 0;
+        sem_wait(&lockEmpty);
 
         //pega a pedra e joga ela no buffer
         bufferRock[posWriteBuffer] = pd_read();
 
-        //incrementa o contador
-        CountRock(1);
+        //incrementa o posWrite
+        posWriteBuffer = (posWriteBuffer + 1) % PROCESSORS_COUNT;
 
+        /*pthread_mutex_lock(&lockCounter);
+        countRock++;
+        pthread_mutex_unlock(&lockCounter);*/
+
+        sem_post(&lockFull);
         
     }
 
+    //sai da thread
+    pthread_exit(NULL);
     return NULL;
 }
-
-
+/*************************************************************/
 //threads consumidoras (UPPs)
 static void* work(void* ignored) {
 
     while ( cancelled == 0 ) {
 
-        //trava a thread se o buffer estiver vazio
-        while(countRock <= 0);
+
+        sem_wait(&lockFull);
+        pthread_mutex_lock(&lockWorker);
 
         //incrementa o posRead
-        posReadBuffer++;
-        if ( posReadBuffer == PROCESSORS_COUNT ) posReadBuffer = 0;
+        posReadBuffer = (posReadBuffer + 1) % PROCESSORS_COUNT;
+        
+        /*pthread_mutex_lock(&lockCounter);
+        countRock--;
+        printf("%d\n", countRock );
+        pthread_mutex_unlock(&lockCounter);*/
+
+        pthread_mutex_unlock(&lockWorker);
+        sem_post(&lockEmpty);
 
         //processa a pedra do buffer e transforma em óleo
         oil_t oil = pd_process(bufferRock[posReadBuffer]);
-        
-        //decrementa o contador
-        CountRock(-1);
+    
 
         //trava as outras threads enquanto pd_deliver() executa
         pthread_mutex_lock(&lockDeliver);
         pd_deliver(oil);
         pthread_mutex_unlock(&lockDeliver);
-
     }
+
+    //sai da thread
+    pthread_exit(NULL);
     return NULL;
 }
-
-/************************
+/*************************************************************
+*
 *   Funções do módulo
-************************/
-
+*
+/*************************************************************/
 void mod_setup() {
 
     assert(setup == 0);
     setup = 1;
     cancelled = 0;
 
+
     //inicia as variáveis mutex
     pthread_mutex_init(&lockDeliver, NULL);
     pthread_mutex_init(&lockCounter, NULL);
-   
+    pthread_mutex_init(&lockWorker, NULL);
 
+
+    //inicia os semáforos
+    sem_init(&lockFull, 0, 0);
+    sem_init(&lockEmpty, 0, PROCESSORS_COUNT);
+ 
 
     //cria as threads
     pthread_create(&prodThread, NULL, &prod, NULL);
@@ -119,13 +120,11 @@ void mod_setup() {
     }
    
 }
-
-
+/*************************************************************/
 char* mod_name() {
     return "grupo9";
 }
-
-
+/*************************************************************/
 void mod_shutdown() {
     assert(setup == 1);
     cancelled = 1;
@@ -140,5 +139,9 @@ void mod_shutdown() {
     //destroi as variáveis mutex
     pthread_mutex_destroy(&lockDeliver);
     pthread_mutex_destroy(&lockCounter);
+
+    //destroi os semáforos
+    sem_destroy(&lockEmpty);
+    sem_destroy(&lockFull);
 }
 
